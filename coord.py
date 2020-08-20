16341cc658e76c12a98df469443e6380cd101f27
wangy <wangyunze412@gmail.com>
2020-08-20 19:52:08 +0800

    generateShips static method should now work

--- coord.py
+++ coord.py
@@ -2,3 +2,9 @@ class Coord:
     def __init__(self, x: int, y: int) -> None:
         self.x = x
         self.y = y
+
+    def __str__(self) -> str:
+        return f"(x: {self.x}, y: {self.y})"
+
+    def __repr__(self) -> str:
+        return self.__str__()
--- ship.py
+++ ship.py
@@ -5,13 +5,22 @@ from coord import Coord
 
 
 class Ship:
-    def __init__(self, length: int) -> None:
-        self.segments: List[Coord] = []
-        self.length: int = length
+    def __init__(self, segments: List[Coord]) -> None:
+        self.segments: List[Coord] = segments
+
+    def __str__(self) -> str:
+        return str(self.segments)
+
+    def __repr__(self) -> str:
+        return self.__str__()
 
     @staticmethod
-    def generateBoard(*sizes) -> List[Coord]:
+    def generateShips(*sizes) -> list:
+        ships = []
         for ship_size in sizes:
+            if ship_size <= 0:
+                raise Exception("Ship size must be > 0")
+
             # Pick a orientation
             orientation = random.choice(("vertical", "horizontal"))
 
@@ -19,4 +28,15 @@ class Ship:
             if orientation == "vertical":
                 # Pick a random starting point
                 starting = Coord(random.randint(0, config.GRID_X - 1),
-                                 random.randint(0, config.GRID_Y - ship_size))
+                                 random.randint(0, config.GRID_Y - 1 - (ship_size - 1)))
+                for i in range(ship_size):
+                    segments.append(Coord(starting.x, starting.y + i))
+            else:
+                starting = Coord(random.randint(0, config.GRID_X - 1 - (ship_size - 1)),
+                                 random.randint(0, config.GRID_Y - 1))
+                for i in range(ship_size):
+                    segments.append(Coord(starting.x + i, starting.y))
+
+            ships.append(Ship(segments))
+
+        return ships
